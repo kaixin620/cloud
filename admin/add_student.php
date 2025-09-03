f81538261cec745032929b786a98372aad07ff69
kaixin620 <132635621+kaixin620@users.noreply.github.com>
2025-09-03 17:10:23 +0800

    Add files via upload

---
+++ admin/add_student.php
@@ -0,0 +1,150 @@
+<?php
+$_title = "Add Student";
+require_once '../_base.php';
+checkSuperadmin();
+
+include 'admin_header.php';
+if (isset($_POST['cancel'])) {
+    echo "<script>window.location.href = 'display_staff.php';</script>";
+}
+
+$_err = [];
+
+
+if (is_post()) {
+    // get data filled in the form
+    $sname = post("sname") ?? "";
+    $semail = post("semail") ?? "";
+    $sphone = post("sphone") ?? "";
+    $saddress = post("saddress") ?? "";
+    $scity = post("scity") ?? "";
+    $sstate = post("sstate") ?? "";
+
+    // validate data
+    $_err["sname"] = checkUsername($sname) ?? '';
+    $_err["semail"] = checkRegisterEmail($semail) ?? '';
+    $_err["sphone"] = checkRegisterContact($sphone) ?? '';
+    $_err["saddress"] = checkAddress($saddress) ?? '';
+    $_err["scity"] = checkCity($scity) ?? '';
+    $_err["sstate"] = checkState($sstate) ?? '';
+
+    // file upload
+    if (isset($_FILES['spic'])) {
+        $file = $_FILES['spic'];
+        $_err["spic"] = checkUploadPic($file);
+
+        // no error
+        if (empty($_err["spic"])) {
+            // everything okay, save the file
+            // create a unique id and use it as file name
+            $ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+            $newFileName = isset($_FILES["spic"]) && !empty($_FILES["spic"] && !empty($ext)) ? uniqid() . '.' . $ext : null;
+        } else {
+            $newFileName = null;
+        }
+    }
+
+    $_err = array_filter($_err);
+
+    // no error then store new student record
+    if (empty($_err)) {
+        $stmt = $_db->prepare("INSERT INTO student (studName, studPic, studEmail, studPhone, studAddress, studCity, studState) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$sname, $newFileName, $semail, $sphone, $saddress, $scity, $sstate]);
+
+        if ($stmt->rowCount() > 0) {
+            // success
+            // save the profile image file
+            if ($newFileName !== null) {
+                move_uploaded_file($file['tmp_name'], '../profilePic/' . $newFileName);
+            }
+            
+            alert_msg('New student record added successfully!', 'student_list.php');
+            exit(); 
+        } else {
+            // fail
+            $_err[] = "Unable to insert. Please try again.";
+        }
+    }
+}
+?>
+
+<head>
+    <link href="../css/login.css" rel="stylesheet" type="text/css" />
+</head>
+
+<style>
+    #save {
+        width: 100%;
+        height: 35px;
+        line-height: 1.5;
+        font-size: 14px;
+        font-family: inherit;
+        background: #373737;
+        color: white;
+        border-radius: 10px;
+        padding: 5px 20px;
+    }
+</style>
+
+<div class="add-staff-box w-100">
+    <div class="add-staff w-60" style="margin: 20px auto;">
+        <h1 class="title text-center">Enter Student Information</h1>
+        <div class="main-content">
+            <form id="reg" method="post" action="" name="reg" enctype="multipart/form-data">
+
+
+                <div class="input-field">
+                    <label for="sname" class="required">Full Name</label>
+                    <?= html_text('sname', "placeholder='Enter Full Name' required") ?>
+                    <?= err("sname") ?>
+                </div>
+                <div class="input-field">
+                    <label for="semail" class="required">Email Address</label>
+                    <?= html_text('semail', "placeholder='Enter Email (e.g. xxxx@xxx.xxx)' required") ?>
+                    <?= err("semail") ?>
+                </div>
+                <div class="input-field">
+                    <label for="sphone" class="required">Mobile</label>
+                    <?= html_text('sphone', "placeholder='Enter Mobile Number (e.g. 0123456789)' required") ?>
+                    <?= err("sphone") ?>
+                </div>
+                <div class="input-field">
+                    <label for="saddress" class="required">Address</label>
+                    <?= html_text('saddress', "placeholder='Enter Address' required") ?>
+                    <?= err("saddress") ?>
+                </div>
+                <div class="input-field">
+                    <label for="scity" class="required">City</label>
+                    <?= html_text('scity', "placeholder='Enter City' required") ?>
+                    <?= err("scity") ?>
+                </div>
+                <div class="input-field">
+                    <label for="sstate" class="required">State</label>
+                    <?= html_text('sstate', "placeholder='Enter State' required") ?>
+                    <?= err("sstate") ?>
+                </div>
+
+                <!-- upload profile pic -->
+                <div class="input-field">
+                    <label for="spic">Profile Picture</label>
+                    <div class="custom-file-button">
+                        <?= html_file('spic', 'image/*') ?>
+                        <label for="spic">Upload Image ... </label>
+                    </div>
+                    <?= err('spic') ?>
+                    <!-- photo preview -->
+                    <label id="upload-preview" tabindex="0">
+                        <img src="../profilePic/profile.png">
+                    </label>
+                </div>
+                <!-- ----------------------------- -->
+                <!-- submit button -->
+                <div class="submit-button">
+                    <input type="submit" value="Save" name="save" id="save" class="form-button" />
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include "admin_footer.php" ?>
---
+++ admin/edit_student.php
@@ -0,0 +1,146 @@
+<?php
+$_title = "Edit Student";
+require_once "../_base.php";
+include "admin_header.php";
+// check if the logged in user is superadmin
+checkSuperadmin();
+
+// get method -- check if the url have id
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    if (is_exists($id, 'student', 'studid')) {
+        $err = [];
+        if (is_post()) {
+            $student = $_db->query("SELECT * FROM student WHERE studid = '$id'")->fetch();
+
+            $studName = post("sname") ?? "";
+            $studEmail = post("semail") ?? "";
+            $studPhone = post("sphone") ?? "";
+            $studAddress = post("saddress") ?? "";
+            $studCity = post("scity") ?? "";
+            $studState = post("sstate") ?? "";
+
+            if ($studName != $student->studName) $_err["sname"] = checkUsername($studName) ?? '';
+            if ($studEmail != $student->studEmail) $_err["semail"] = checkRegisterEmail($studEmail) ?? '';
+            if ($studPhone != $student->studPhone)  $_err["sphone"] = checkContact($studPhone) ?? '';
+            if ($studAddress != $student->studAddress)  $_err["saddress"] = checkAddress($studAddress) ?? '';
+            if ($studCity != $student->studCity)  $_err["scity"] = checkCity($studCity) ?? '';
+            if ($studState != $student->studState)  $_err["sstate"] = checkState($studState) ?? '';
+
+            if (isset($_FILES['spic'])) {
+                $file = $_FILES['spic'];
+                $_err["spic"] = checkUploadPic($file);
+
+                // no error
+                if (empty($_err["spic"])) {
+                    // everything okay, save the file
+                    // create a unique id and use it as file name
+                    $ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+                    $newFileName = isset($_FILES["spic"]) && !empty($_FILES["spic"] && !empty($ext)) ? uniqid() . '.' . $ext : $student->studPic;
+                } else {
+                    $newFileName = $student->studPic;
+                }
+            }
+
+            $_err = array_filter($_err);
+
+            // store new student record
+            if (empty($_err)) {
+                $stmt = $_db->prepare("UPDATE student SET studName = ?, studPic = ?, studEmail = ?, studPhone = ?, studAddress = ?, studCity = ?, studState = ? WHERE studid = ?");
+                $stmt->execute([$studName, $newFileName, $studEmail, $studPhone, $studAddress, $studCity, $studState, $id]);
+                if ($stmt->rowCount() < 1) {
+                    alert_msg("Unable to update details. Please try again.");
+                } else {
+                    //save the file
+                    if ($newFileName != $student->studPic) {
+                        move_uploaded_file($file['tmp_name'], '../profilePic/' . $newFileName);
+                        // delete old pic from file
+                        if($student->studPic != null){
+                            unlink("../profilePic/$student->studPic");
+                        }
+                    }
+                    alert_msg('Record update successful');
+                }
+            }
+        }
+    } else {
+        alert_msg('Student not exist.', $_SERVER['HTTP_REFERER']);
+    }
+} else {
+    alert_msg("No student selected.", $_SERVER['HTTP_REFERER']);
+}
+
+$result = $_db->query("SELECT * FROM student WHERE studid = '$id'");
+$s = $result->fetch();
+
+$sname = $s->studName;
+$semail = $s->studEmail;
+$sphone = $s->studPhone;
+$saddress = $s->studAddress;
+$scity = $s->studCity;
+$sstate = $s->studState;
+?>
+
+<head>
+    <link href="../css/login.css" rel="stylesheet" type="text/css" />
+    <link href="../css/edit_staff.css" rel="stylesheet" type="text/css" />
+</head>
+
+<form method="post" enctype="multipart/form-data">
+    <div class="form-box">
+        <div class="photo-area">
+            <div class="photo">
+                <!-- photo preview -->
+                <label id="upload-preview" tabindex="0">
+                    <?= html_file('spic', 'image/*', 'hidden') ?>
+                    <img src="../profilePic/<?= $s?->studPic ?? 'profile.png'?>">
+                    <span>Upload Profile Picture</span>
+                </label>
+                <?= err('spic') ?>
+            </div>
+        </div>
+        <div class="data-area">
+            <div class="data">
+                <div class="input-field">
+                    <div>Student ID : <?= $s->studid ?></div>
+                </div>
+                <div class="input-field">
+                    <label for="sname" class="required">Full Name</label>
+                    <?= html_text('sname', "placeholder='Enter Full Name'") ?>
+                    <?= err("sname") ?>
+                </div>
+                <div class="input-field">
+                    <label for="semail" class="required">Email Address</label>
+                    <?= html_text('semail', "placeholder='Enter Email (e.g. xxxx@xxx.xxx)'") ?>
+                    <?= err("semail") ?>
+                </div>
+                <div class="input-field">
+                    <label for="sphone" class="required">Mobile</label>
+                    <?= html_text('sphone', "placeholder='Enter Mobile Number (e.g. 0123456789)'") ?>
+                    <?= err("sphone") ?>
+                </div>
+                <div class="input-field">
+                    <label for="saddress" class="required">Address</label>
+                    <?= html_text('saddress', "placeholder='Enter Address'") ?>
+                    <?= err("saddress") ?>
+                </div>
+                <div class="input-field">
+                    <label for="scity" class="required">City</label>
+                    <?= html_text('scity', "placeholder='Enter City'") ?>
+                    <?= err("scity") ?>
+                </div>
+                <div class="input-field">
+                    <label for="sstate" class="required">State</label>
+                    <?= html_text('sstate', "placeholder='Enter State'") ?>
+                    <?= err("sstate") ?>
+                </div>
+                <!-- submit button -->
+                <div class="submit-button">
+                    <input type="submit" class="form-button" value="Save" name="save" id="save" />
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+<?php include "admin_footer.php" ?>
