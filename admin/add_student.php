dfc1e1980770c60049c920acc46d180dcee4cf69
kaixin620 <xinkteoh@gmail.com>
2025-09-05 18:38:06 +0800

    change S3

--- admin/add_student.php
+++ admin/add_student.php
@@ -23,7 +23,7 @@ $s3Client = new S3Client([
 ]);
 
 // Bucket name in S3
-$bucketName = 'assm-student-image-bucket';  
+$bucketName = 'assm-student-web-bucket';  
 
 if (is_post()) {
     // get data filled in the form
